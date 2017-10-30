11cd3c213ce05f2c0fcf6988f8fac885b22780af
konglee <konglee873@gmail.com>
2017-10-30 19:56:58 +0800

    add code

---
+++ RunloopDeepUseage/Tools/SymbolicTest.h
@@ -0,0 +1,15 @@
+//
+//  SymbolicTest.h
+//  RunloopDeepUseage
+//
+//  Created by konglee on 2017/10/28.
+//  Copyright © 2017年 konglee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SymbolicTest : NSObject
+
++ (void)test;
+
+@end
